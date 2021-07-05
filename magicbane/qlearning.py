9f2fcc9463f8ceb2268717c4afdf79c2a1bee3ec
Scott Hellman <scotthellman2@gmail.com>
2021-07-04 20:16:40 -0500

    trying out a correct implementation of qlambda, seems better

--- magicbane/qlearning.py
+++ magicbane/qlearning.py
@@ -17,21 +17,19 @@ class QLearner:
     def step(self, new_state, last_reward):
         if last_reward is not None:
             future_reward = np.max(self.Q[new_state])
-            # q goes negative on NCHain-v0 which has only positive rewards, this is obviously not right
-            td_term = last_reward + self.gamma * future_reward - self.Q[self.last_state, self.last_action]
-            trace_term = self.eligibility*(td_term - self.Q)
+            delta = last_reward + self.gamma * future_reward - self.Q[self.last_state, self.last_action]
+            trace_term = self.eligibility*delta
             self.Q += self.alpha*trace_term
 
         if np.random.random() > self.epsilon:
             next_action = np.random.randint(self.Q.shape[1])
-
             # can't trace past exploration
             self.eligibility *= 0
         else:
             next_action = np.argmax(self.Q[new_state])
-            self.eligibility *= self.lam * self.gamma
+            self.eligibility *= self.lam
 
-        self.eligibility[new_state, next_action] = 1
+        self.eligibility[new_state, next_action] += 1
         self.last_state = new_state
         self.last_action = next_action
         return next_action
@@ -40,17 +38,16 @@ class QLearner:
 if __name__ == "__main__":
     import gym
 
-
     env = gym.make("NChain-v0")
-    learner = QLearner(env.observation_space.n, env.action_space.n, epsilon=0.75)
-    obs = env.reset()
-    reward = None
-    done = False
-    for i in range(100):
-        if i % 25 == 0:
-            print(learner.Q)
-        action = learner.step(obs, reward)
-        if done:
-            break
-        obs, reward, done, info = env.step(action)
+    learner = QLearner(env.observation_space.n, env.action_space.n, alpha=0.05, gamma=0.9, lam=0.5, epsilon=0.9)
+    for episode in range(20):
+        print(learner.Q)
+        obs = env.reset()
+        reward = None
+        done = False
+        for i in range(1000):
+            action = learner.step(obs, reward)
+            if done:
+                break
+            obs, reward, done, info = env.step(action)
     env.close()
